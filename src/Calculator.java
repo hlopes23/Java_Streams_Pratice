515aec5d3eb6e4199ebe26b3263496040a4a326c
mindera teste <henrique.silva.lopes@outlook.pt>
2024-12-17 16:26:03 +0000

    Another test commit

---
+++ src/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+    //definir caracteristicas do objeto Calculator
+
+    int firstnumber;
+    int secondnumber;
+
+    public int sum () {
+      return firstnumber + secondnumber;
+
+    }
+
+    public int subtract () {
+        return firstnumber - secondnumber;
+
+    }
+
+    public int multiply () {
+        return firstnumber * secondnumber;
+
+    }
+
+    public int division () {
+        return firstnumber/secondnumber;
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator Calculator = new Calculator();
+
+        Calculator.firstnumber = 93931;
+        Calculator.secondnumber = -1;
+
+        System.out.println(Calculator.subtract());
+
+    }
+}
+
